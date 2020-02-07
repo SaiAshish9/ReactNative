8a3857f4fb68a1e7081dc222cd922ee5d1bd4d5d
SaiAshish9 <43849911+SaiAshish9@users.noreply.github.com>
2020-02-07 14:18:57 +0530

    Create flatview.js

---
+++ flatview.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import {
+StyleSheet,
+Text,
+View,
+Image,
+// ScrollView,
+FlatList
+} from 'react-native';
+
+import getReviews from './reviews'
+
+
+function Review({name,text,avatar}){
+  return (
+    <View>
+      <Image  source={{uri:avatar}} style={styles.avatar} />
+      <View>
+        <Text>
+          {name}
+          </Text>
+          <Text>
+            {text}
+        </Text>
+      </View>
+    </View>
+  )
+}
+
+export default class App extends React.Component {
+
+componentDidMount(){
+  console.log("started");
+}
+
+renderItem=({item})=>{
+  return <Review {...item}/>
+
+}
+
+
+
+  render(){
+    const reviews=getReviews();
+
+   
+    return (
+      <View style={styles.container}>
+        <FlatList
+        
+        data={reviews}
+        renderItem={this.renderItem}
+        />
+  
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+container:{
+  flex:1
+},
+avatar:{
+  height:50,
+  width:50,
+  borderRadius:25
+}
+});
