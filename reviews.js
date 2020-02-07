73d065735d59a532d23e2c92a83809f45b326237
SaiAshish9 <43849911+SaiAshish9@users.noreply.github.com>
2020-02-07 22:53:34 +0530

    Add files via upload

---
+++ reviews.js
@@ -0,0 +1,59 @@
+const reviews=[
+
+    {
+        "name":"Sai",
+        "text":"hi i'm sai",
+        "avatar":"https://github.com/saiashish9.png?size=32"
+    },
+    {
+        "name":"Sai",
+        "text":"hi i'm sai",
+        "avatar":"https://github.com/saiashish9.png?size=32"
+    },    {
+        "name":"Sai",
+        "text":"hi i'm sai",
+        "avatar":"https://github.com/saiashish9.png?size=32"
+    },    {
+        "name":"Sai",
+        "text":"hi i'm sai",
+        "avatar":"https://github.com/saiashish9.png?size=32"
+    },    {
+        "name":"Sai",
+        "text":"hi i'm sai",
+        "avatar":"https://github.com/saiashish9.png?size=32"
+    },    {
+        "name":"Sai",
+        "text":"hi i'm sai",
+        "avatar":"https://github.com/saiashish9.png?size=32"
+    },    {
+        "name":"Sai",
+        "text":"hi i'm sai",
+        "avatar":"https://github.com/saiashish9.png?size=32"
+    },    {
+        "name":"Sai",
+        "text":"hi i'm sai",
+        "avatar":"https://github.com/saiashish9.png?size=32"
+    },    {
+        "name":"Sai",
+        "text":"hi i'm sai",
+        "avatar":"https://github.com/saiashish9.png?size=32"
+    },    {
+        "name":"Sai",
+        "text":"hi i'm sai",
+        "avatar":"https://github.com/saiashish9.png?size=32"
+    },    {
+        "name":"Sai",
+        "text":"hi i'm sai",
+        "avatar":"https://github.com/saiashish9.png?size=32"
+    },    {
+        "name":"Sai",
+        "text":"hi i'm sai",
+        "avatar":"https://github.com/saiashish9.png?size=32"
+    }
+];
+
+
+
+export default function getReviews(){
+    return reviews
+}
