d7c4ab7fb6d31e48ecb35e0e8f94c3155e45e2f0
SaiAshish9 <43849911+SaiAshish9@users.noreply.github.com>
2020-02-07 22:52:20 +0530

    Create keyboardAvoidingView.js

---
+++ keyboardAvoidingView.js
@@ -0,0 +1,63 @@
+import {
+StyleSheet,
+Text,
+View,
+KeyboardAvoidingView,
+Switch,
+TextInput
+} from 'react-native';
+
+
+export default class App extends React.Component {
+
+state={
+    input:'sdfghjkl',
+    showInput:false
+}
+
+handleToggle=()=>{
+    this.setState((state)=>({
+        showInput:!state.showInput
+    }))
+}
+
+handleChange=(input)=>{
+    this.setState((state)=>({
+          input
+    }))
+}
+
+
+  render(){
+    const reviews=getReviews();
+
+    return (
+      <View style={styles.container}>
+      
+      <KeyboardAvoidingView behaviour="padding"  >
+
+       <Switch
+       value={showInput}
+       onValueChange={this.handleToggle}       
+       />
+{showInput===true&&(
+    <TextInput
+    value={input}
+    onChange={this.handleChange}    
+    
+    
+    />
+)}
+
+</KeyboardAvoidingView>
+
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+container:{
+  flex:1
+}
+});
